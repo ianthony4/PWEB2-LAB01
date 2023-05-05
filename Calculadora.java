46e4d162467113f86f6d7f7fd311336bde52d2f0
anthony chaisa <achaisa@unsa.edu.pe>
2023-05-05 12:03:43 -0500

    Solicitando al usuario los 2 numeros

--- Calculadora.java
+++ Calculadora.java
@@ -4,6 +4,11 @@ public class Calculadora {
     public static void main(String[] args){
         int a,b;
         Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el primer numero: ");
+        a = sc.nextInt();
+        System.out.println("Ingrese el segundo numero: ");
+        b = sc.nextInt();
+    
     }   
     public int add(int a, int b){
         return a+b;
