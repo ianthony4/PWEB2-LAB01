cece2183a93db04b4daf79f9ab5540093e9e33f4
anthony chaisa <achaisa@unsa.edu.pe>
2023-05-05 11:58:35 -0500

    agregando un lector (scanner) y 2 variables para el metodo main

--- Calculadora.java
+++ Calculadora.java
@@ -1,8 +1,9 @@
 package Lab01a;
-
+import java.util.Scanner;
 public class Calculadora {
     public static void main(String[] args){
-
+        int a,b;
+        Scanner sc = new Scanner(System.in);
     }   
     public int add(int a, int b){
         return a+b;
