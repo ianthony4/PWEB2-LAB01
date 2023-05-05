ab731f88a06d329e8584d980b359556a108bd17a
anthony chaisa <achaisa@unsa.edu.pe>
2023-05-05 11:38:50 -0500

    agregando la funcion suma

--- Calculadora.java
+++ Calculadora.java
@@ -2,6 +2,9 @@ package Lab01a;
 
 public class Calculadora {
     public static void main(String[] args){
-        
-    }    
+
+    }   
+    public int add(int a, int b){
+        return a+b;
+    } 
 }
